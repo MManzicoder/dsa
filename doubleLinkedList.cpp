455e753e4b847c18689744169bf0030d0d54fe97
Mmanzi monnierey <mudahemukamanzi@yahoo.com>
2022-03-03 15:20:57 +0200

    full functioning double-linked-list

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -14,13 +14,16 @@ struct Node {
 struct DoubleLinkedList{
     public:
         Node * head;
+        Node *tail;
         DoubleLinkedList(){
             this->head = NULL;
+            this->tail = NULL;
         }
         Node *addBefore(int data){
             Node *newNode = new Node(data);
             if(head == NULL){
                 head = newNode;
+                tail=newNode;
                 return newNode;
             }
             head->prev = newNode;
@@ -43,22 +46,61 @@ struct DoubleLinkedList{
         Node * newNode = new Node(data);
         if(head == NULL){
             head = newNode;
+            tail = newNode;
             return newNode;
         }
-        Node *current = head;
-        while(current->next != NULL){
-            current = current->next;
-        }
-        newNode->prev = current;
-        current->next = newNode;
+        newNode->prev = tail;
+        tail->next = newNode;
+        tail = newNode;
         return newNode;
     }
+        void printBackWard(){
+        if(head == NULL){
+            cout<<"Empty List!"<<endl;
+            return;
+        }
+        Node *current = tail;
+         while(current != NULL){
+             cout<<current->data<<" ";
+             current = current->prev;
+         }
+         cout<<endl;
+
+    }
+    void insertAfter(Node *node, int data){
+        if(node == NULL){
+            cout<<"Passed NULL Pointer"<<endl;
+            return;
+        }
+       Node *newNode = new Node(data);
+       if(node->next == NULL){
+           this->addAfter(data);
+           return;
+       }
+       Node *nextNode = node->next;
+       node->next = newNode;
+       newNode->prev = node;
+       newNode->next = nextNode;
+       nextNode->prev = newNode;
+       
+    }
+    Node *getLastNode(){
+        Node *tail = head;
+        while(tail->next !=NULL){
+           tail = tail->next;
+        }
+        return tail;
+    }
+
 };
 void ask(DoubleLinkedList *dl){
     int choice;
-    cout<<"1. Display"<<endl;
+    Node *sliceNode;
+    cout<<"1. Display From head"<<endl;
     cout<<"2. Add Before"<<endl;
     cout<<"3. Add After"<<endl;
+    cout<<"4. Display From Tail"<<endl;
+    cout<<"5. Insert After"<<endl;
     cout<<"0. Quit"<<endl;
     cout<<"Enter your choice: ";
     cin>>choice;
@@ -70,12 +112,20 @@ void ask(DoubleLinkedList *dl){
            int el;
            cout<<"Enter element: ";
            cin>>el;
-           dl->addBefore(el);
+           sliceNode = dl->addBefore(el);
            break;
         case 3:
            cout<<"Enter element: ";
            cin>>el;
            dl->addAfter(el);
+           break;
+        case 4:
+           dl->printBackWard();
+           break;
+        case 5:
+           cout<<"Enter element: ";
+           cin>>el;
+           dl->insertAfter(sliceNode, el);
            break;
         case 0:
           exit(-1);           
